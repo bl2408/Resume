932b202b62328816bccc693879a57eb0099de0ae
Brian Lambert <brianlambert2408@gmail.com>
2022-11-26 14:46:19 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var server = express()
+
+// var path = require('path')
+
+var PORT = 3000
+
+server.use(express.static('public'))
+
+server.listen(PORT, function () {
+  console.log('Listening on port', PORT)
+})
